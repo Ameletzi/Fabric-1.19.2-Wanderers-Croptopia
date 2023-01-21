75c0f39c545e8a17cd9656f0ac26796a8b71db5c
Ameletzi <P#1at@d!stUsTeche=i!>
2023-01-21 12:42:48 +0000

    Did not commit any files the first time around.

---
+++ src/main/java/ameletzi/wandererscroptopia/WanderersCroptopia.java
@@ -0,0 +1,22 @@
+package ameletzi.wandererscroptopia;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WanderersCroptopia implements ModInitializer {
+
+	public static final String MOD_ID = "wanderers-croptopia";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("wanderers-croptopia");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/ameletzi/wandererscroptopia/WanderersCroptopiaClient.java
@@ -0,0 +1,10 @@
+package ameletzi.wandererscroptopia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WanderersCroptopiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
