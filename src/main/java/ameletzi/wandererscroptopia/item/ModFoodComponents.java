c7c8160fe5cfde4961914ff754f7ee852d2c9cff
Ameletzi <P#1at@d!stUsTeche=i!>
2023-01-22 10:21:29 +0000

    Added baps and pasties, along with a cinnamon recipe, and the ability to cook rotten flesh into leather

--- src/main/java/ameletzi/wandererscroptopia/item/ModFoodComponents.java
+++ src/main/java/ameletzi/wandererscroptopia/item/ModFoodComponents.java
@@ -4,7 +4,10 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent BACON_BAP = new FoodComponent.Builder().hunger(10).saturationModifier(0.6f).build();
+    public static final FoodComponent BREAKFAST_BAP = new FoodComponent.Builder().hunger(18).saturationModifier(1.0f).build();
+    public static final FoodComponent EGG_BAP = new FoodComponent.Builder().hunger(10).saturationModifier(0.6f).build();
     public static final FoodComponent SAUSAGE_BAP = new FoodComponent.Builder().hunger(10).saturationModifier(0.6f).build();
     public static final FoodComponent VEGETABLE_PASTY = new FoodComponent.Builder().hunger(10).saturationModifier(0.6f).build();
     public static final FoodComponent SPICY_PASTY = new FoodComponent.Builder().hunger(10).saturationModifier(0.6f).build();
+    public static final FoodComponent BEANS_ON_TOAST = new FoodComponent.Builder().hunger(12).saturationModifier(0.6f).build();
 }
--- src/main/java/ameletzi/wandererscroptopia/item/ModItems.java
+++ src/main/java/ameletzi/wandererscroptopia/item/ModItems.java
@@ -15,12 +15,18 @@ public class ModItems {
             new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU)));
     public static final Item BACON_BAP = registerItem("bacon_bap",
             new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.BACON_BAP)));
+    public static final Item BREAKFAST_BAP = registerItem("breakfast_bap",
+            new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.BREAKFAST_BAP)));
+    public static final Item EGG_BAP = registerItem("egg_bap",
+            new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.EGG_BAP)));
     public static final Item SAUSAGE_BAP = registerItem("sausage_bap",
             new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.SAUSAGE_BAP)));
     public static final Item VEGETABLE_PASTY = registerItem("vegetable_pasty",
             new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.VEGETABLE_PASTY)));
     public static final Item SPICY_PASTY = registerItem("spicy_pasty",
             new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.SPICY_PASTY)));
+    public static final Item BEANS_ON_TOAST = registerItem("beans_on_toast",
+            new Item(new FabricItemSettings().group(ModItemGroup.WANDERERS_MENU).food(ModFoodComponents.BEANS_ON_TOAST)));
 
 
 
